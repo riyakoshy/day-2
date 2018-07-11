0cca43a8bac6c8a6402d8140450950051061a4a7
Riya Koshy <riyakoshy@Riyas-MacBook-Air.local>
2018-07-11 17:06:55 -0400

    new changes

--- exercises/script.js
+++ exercises/script.js
@@ -1,59 +1,74 @@
+
+
 document.querySelector('.puzzle-0').addEventListener('click', function() {
-	  window.alert('Hello, World!');
+	  alert('Hello, World!');
 	});
 
 	document.querySelector('.puzzle-1').addEventListener('click', function() {
-		
+		alert('Bonjour, Monde!')
 	});
 
 	document.querySelector('.puzzle-2').addEventListener('click', function() {
-
+		document.querySelector('h1').textContent = "Grandma Doing Avocado Dance";
 	});
 
 	document.querySelector('.puzzle-3').addEventListener('click', function() {
-
+		document.querySelector('img').src = "https://media.giphy.com/media/liamKgDNyZi3C/giphy.gif";
 	});
 
 	document.querySelector('.puzzle-4').addEventListener('click', function() {
-
+		document.querySelector('img').style.visibility = 'hidden';
 	});
 
 	document.querySelector('.puzzle-5').addEventListener('click', function() {
-
+		document.querySelector('img').style.visibility = 'visible';
 	});
 
 	document.querySelector('.puzzle-6-yellow').addEventListener('click', function() {
-		
+		document.querySelector('html').style.background = "yellow";
 	});
 
 	document.querySelector('.puzzle-6-pink').addEventListener('click', function() {
-		
+		document.querySelector('html').style.background = "pink";
 	});
 
 	document.querySelector('.puzzle-6-purple').addEventListener('click', function() {
-		
+		document.querySelector('html').style.background = "purple";
 	});
 
 	document.querySelector('.puzzle-7').addEventListener('click', function() {
-		
+		document.querySelector('html').style.background = "red";
+		document.querySelector('body').style.fontFamily = 'Comic Sans MS';
 	});
 
 	document.querySelector('.puzzle-8').addEventListener('click', function() {
+		document.querySelector('a').href = "https://www.youtube.com/";
+		document.querySelector('a').textContent = "Go to Youtube";
 
 	});
 
 	// Challenge puzzles
 
-	document.querySelector('.puzzle-9')
+	document.querySelector('.puzzle-9').addEventListener('click', function() {
+		document.querySelector('body').style.fontFamily = 'Monaco';
+	});
 
-	document.querySelector('.puzzle-10')
+	document.querySelector('.puzzle-10').addEventListener('click', function() {
+		var audio = new Audio("Migos - Walk It Talk It ft. Drake.mp3");
+		audio.play()	
+		
+	});
 
-	document.querySelector('.puzzle-11')
+	document.querySelector('.puzzle-11').addEventListener('click', function() {
+		document.querySelector('.puzzle11').innerHTML = "<img src='http://codegakko.com/wp-content/uploads/2016/09/CG-Logo.png'>";
+	});
 
 	document.querySelector('.puzzle-12')
 
 	document.querySelector('.puzzle-13')
 
 	document.querySelector('.puzzle-14')
-
+	
 	document.querySelector('.puzzle-15')
+
+
---
+++ night/script.js
@@ -0,0 +1,15 @@
+// alert("Someone has been MURDERED")
+var isInDarkMode = false;
+
+document.querySelector('button').addEventListener('click', function() {
+	if (isInDarkMode) {
+		document.querySelector('html').style.background = "white";
+		document.querySelector('html').style.color= "black";
+		isInDarkMode = false
+	} else {
+
+	document.querySelector('html').style.background = "black";
+	document.querySelector('html').style.color = "beige";
+	isInDarkMode = true;
+}
+});
